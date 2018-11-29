353bd4aac27850a47cb6ecf1b1008f7b422ed0c1
Meri Saarinen <mtmsaa@utu.fi>
2018-11-29 21:24:43 +0200

    Added list of last 10 searches. Exercise 2 done

--- Exercise 2/js/zcl.js
+++ Exercise 2/js/zcl.js
@@ -1,38 +1,51 @@
+//Saves search and fetches coordinates
+function fetchAndProcess(countryCode, postalCode){
+    var valid = false;
+    (typeof countryCode == 'string' && typeof postalCode == 'integer' ) ? alert('Form not valid') : valid = true
+    if(valid){
+        url = createAPIurl(countryCode, postalCode)
+        fetchData(url)
+    }
+}
+//Fetches data
 function fetchData(url) {
     fetch(url)
     .then( (resp) => resp.json())
     .then ( data => {
-        // console.log(data)
         processData(data);
     })
     .catch( function() {
         console.log('Not found.')
+        alert('Not found')
     })
 }
+//Creates url to fetch the country code and postal code
 function createAPIurl(countryCode, postalCode){
     var url = 'https://api.zippopotam.us/' + countryCode + '/' + postalCode
     return url
 }
-function fetchAndProcess(countryCode, postalCode){
-    console.log(countryCode, ' ', postalCode)
-    saveSearch(countryCode,postalCode)
-    url = createAPIurl(countryCode, postalCode)
-    fetchData(url)
-}
+//Processes data by setting it to the tableInfo table
 function processData(data){
-    console.log(data)
     var table = document.getElementById('tableInfo')
-    console.log(table)
-    console.log(table.children[0])
     var row = table.children[0].children[1];
-    console.log(row)
-    if(typeof data != 'undefined' || data != null){
+    //Check if data contains information
+    if(typeof data.country == 'string'){
+        console.log(data)
         row.children[0].innerHTML = data.country
         row.children[1].innerHTML = data.places[0].longitude
         row.children[2].innerHTML = data.places[0].latitude
+        saveSearch(data.country,data['post code'])
+        updateSearchedList();
+        findOnMap(data.places[0].latitude, data.places[0].longitude)
+   //Else empty information row
+    } else {
+        for(var i = 0; i<3; i++){
+            row.children[i].innerHTML = ''
+        }
     }
-    findOnMap(data.places[0].latitude, data.places[0].longitude)
+   
 }
+//Creates google map
 function myMap() {
     var mapProp= {
         center:new google.maps.LatLng(51.508742,-0.120850),
@@ -40,51 +53,56 @@ function myMap() {
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+//Finds the postal code in the map and zooms in it
 function findOnMap(latitude, longitude) {
-    var mapProp= {
-        center:new google.maps.LatLng(latitude, longitude),
-        zoom:15,
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    
+    var uluru = new google.maps.LatLng(latitude,longitude);
+    var map=new google.maps.Map(document.getElementById("googleMap"),{zoom: 15, center: uluru});
+    var marker = new google.maps.Marker({position: uluru, map: map});
 }
-// function saveSearch(search){
-//     var list = [search];
-//     if(getCookie('searchList') != ""){
-//         // console.log('get cookie ', getCookie('search'))
-//         list.push(getCookie('search'))}
-//         // console.log('ok')
-//     console.log('list ', list)
-//     var json_str = JSON.stringify(list);
-//     console.log(json_str)
-//    setCookie('search', json_str, 10)
-//    console.log('get cookie ', getCookie('searchList'))
-// }
+
+//Saves searches to local storage
 function saveSearch(country, pc){
-    localStorage.setItem(country, pc);
+    let id = (localStorage.getItem('latestId') === null) ? 0 : localStorage.getItem('latestId');
+    id ++;
+    localStorage.setItem('latestId', id)
+    console.log(id)
+    var list = country+','+pc
+    localStorage.setItem(id, list);
 }
+//Sets searched list
+function setSearchedListToDiv(){
+    var s =document.getElementById('searched').getElementsByTagName('ul')[0]
+    console.log(s)
+    let number = (localStorage.getItem('latestId') === null) ? 0 : localStorage.getItem('latestId');
+    console.log(number)
+    for(var i = number; i>number-10 && i>=1; i--){
+        var listObject = document.createElement('li')
+        listObject.innerHTML = localStorage.getItem(i)
+        s.append(listObject)
+    }
+}
+//Updates searched list
+function updateSearchedList(){
+    console.log('updating list')
+    var s =document.getElementById('searched').getElementsByTagName('ul')[0]
+    let number = (localStorage.getItem('latestId') === null) ? 0 : localStorage.getItem('latestId');
+    if(s.childNodes.length<10){
+            var listObject = document.createElement('li')
+            listObject.innerHTML = localStorage.getItem(number)
+            s.append(listObject)
+    } else {
+        for(var i = 9; i>=0; i--){
+            var listObject = s.childNodes[i]
+            listObject.innerHTML = localStorage.getItem(number)
+            number--
+            console.log(number)
+            s.append(listObject)
+        }
+    }
+}
+//Run these functions when the page loads
 function start(){
     myMap();
-    // fetchCountries();
+    setSearchedListToDiv();
 }
-// function setCookie(cname, cvalue, exdays) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//     var expires = "expires="+d.toUTCString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
-
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var ca = document.cookie.split(';');
-//     for(var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
