24993c3f4e863d1737b45420ce4f34a5c408f2b7
Meri Saarinen <mtmsaa@utu.fi>
2018-11-30 22:52:22 +0200

    Added hide button and centered barcode

--- Exercise 1/js/virtualBarcode.js
+++ Exercise 1/js/virtualBarcode.js
@@ -9,7 +9,8 @@ function greyOut(event) {
 function processInformation(vbcElement){
     var vc = vbcElement.value;
     vc = vc.trim()
-    if(vc.length =! 54){
+    console.log(vc.length)
+    if(vc.length != 54){
         alert("Virtual code invalid")
     }else{
         var IBAN = vc.substring(1,17)
@@ -18,14 +19,16 @@ function processInformation(vbcElement){
         var expiresRaw = vc.substring(vc.length-6,vc.length)
         var date = processDate(expiresRaw)
         RN = cleanString(RN)
+        console.log(RN)
         total = cleanString(total)
         var list = [IBAN, total, RN, date]
         setDatatoTable(document.getElementById('dT'), list)
-        setBarCode()
+        setBarCode(vc)
     }
 }
-function setBarCode(){
-    JsBarcode("#barcode", "Hi!");
+//Sets bar code
+function setBarCode(vc){
+    JsBarcode("#barcode", vc);
 }
 //Processes date to string 
 function processDate(raw){
@@ -50,10 +53,20 @@ function setDatatoTable(table, list){
         tab[i].innerHTML = v + ': ' + list[i]
     }
 }
-
 //Cleans string of zeros from the front
 function cleanString(raw){
-    cData = parseInt(raw, 0).toString()
+    cData = parseInt(raw, 0)
+    cData = '' + cData
     return cData
-    
+}
+//Toggles element hide
+function hide(event, element){
+    if (element.style.display === "none") {
+        element.style.display = "block";
+        console.log(event.target)
+        event.target.innerHTML = 'Hide'
+    } else {
+        element.style.display = "none";
+        event.target.innerHTML = 'Show'
+    }
 }
